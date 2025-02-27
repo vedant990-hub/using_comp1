904e057cd4bcda20a2cb02712459deb22105d087
vedant990-hub <vedantpawar990@gmail.com>
2025-02-27 11:20:28 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import BookCard from "./components/bookcard.jsx";
+import './App.css';
 
 function App() {
   const books = [
@@ -34,7 +36,13 @@ function App() {
     },
   ];
 
-  return <>{/* write code to display all the books using */}</>;
+  return (
+    <div className="book-container">
+      {books.map((book) => (
+        <BookCard key={book.id} book={book} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
--- src/components/bookcard.jsx
+++ src/components/bookcard.jsx
@@ -1,1 +1,18 @@
 // write the book component code here
+import React from 'react';
+import './BookCard.css';
+
+const BookCard = ({ book }) => {
+  return (
+    <div className="book-card">
+      <img src={book.image} alt={book.name} className="book-image" />
+      <div className="book-details">
+        <h3 className="book-title">{book.name}</h3>
+        <p className="book-author">Author: {book.author}</p>
+        <p className="book-genre">Genre: {book.genre}</p>
+      </div>
+    </div>
+  );
+};
+
+export default BookCard;
